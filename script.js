c8c6035343a126c019efba55ba33bc04e0940d6b
AbdramaneDiallo777 <abdramane.diallo@edu.ece.fr>
2026-02-18 17:44:51 +0100

    Premier envoi

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scroll pour la nav
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 80,
+        behavior: "smooth"
+      });
+    }
+  });
+});
